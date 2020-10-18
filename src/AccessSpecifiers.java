b4b86bfc4c8be87519d9429ddf6132a3703ff0c6
ktigist <ktigist19@gmail.com>
2020-10-18 11:42:27 -0500

    initial commit

---
+++ src/AccessSpecifiers.java
@@ -0,0 +1,11 @@
+public class AccessSpecifiers {
+
+    // class, variable, methods
+    // public --> everywhere
+    // private --> same class
+    // default --> same package & different package's subclass
+    // protected --> same package
+
+
+
+}
---
+++ src/com/company/Demo.java
@@ -0,0 +1,25 @@
+public class Demo {
+    public static void main(String[] args) {
+        double e = 2;
+        double f = 4;
+        double g = 6;
+        double h = e + f + g;
+        System.out.println(h);
+        boolean isCloudyToday = true;
+        System.out.println("is Cloudy Today?");
+        System.out.println(isCloudyToday);
+
+        boolean javaisveryeasytolearn = false;
+        System.out.println("java is very easy to learn?");
+        System.out.println(javaisveryeasytolearn);
+
+        boolean rightnowweareinjavasubject = true;
+        System.out.println("right now we are in java subject ?");
+        System.out.println(rightnowweareinjavasubject);
+
+        boolean tigistisqatestingstudent = true;
+        System.out.println("tigist is qa testing student? ");
+        System.out.println(tigistisqatestingstudent);
+    }
+}
+
